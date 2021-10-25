e6c058dfb7c40af7d4c24e34df1bc92bfe369d59
baekjiuk <jiuk205@naver.com>
2021-10-25 18:55:47 +0900

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+/* eslint-disable @next/next/no-sync-scripts */
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+const client = new ApolloClient({
+  uri: "localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <script src="/smarteditor/js/service/HuskyEZCreator.js" charSet="utf-8"></script>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
-export default MyApp
+export default MyApp;

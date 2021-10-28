4af29c1f3e4ff951ef73311cf79a85274e522a80
baekjiuk <jiuk205@naver.com>
2021-10-28 12:14:15 +0900

    modify footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -13,46 +13,68 @@ const Footer = () => (
         </div>
       </Link>
     </div>
-    <div className={styles.sns}>
-      <div
-        onClick={() =>
-          openLink("https://www.facebook.com/profile.php?id=100073682116389")
-        }
-      >
-        <Image src="/images/icon-fb.png" width="70" height="70" />
-      </div>
-      <div
-        onClick={() => openLink("https://www.instagram.com/beta_festival_/")}
-      >
-        <Image src="/images/icon-insta.png" width="70" height="70" />
-      </div>
-      <div
-        onClick={() =>
-          openLink("https://www.youtube.com/channel/UCX7IRsH1eGOruoGZhY-kebw")
-        }
-      >
-        <Image src="/images/icon-youtube.png" width="70" height="70" />
-      </div>
-    </div>
-    <div>
-      <div className={styles.locale}>
-        <div onClick={() => openLink("https://www.andong.go.kr/main.do")}>
-          주최 ·
-          <div className={styles.localeImage}>
-            <Image src="/images/icon-locale1.png" width="139" height="33" />
-          </div>
+    <div className={styles.center}>
+      <div className={styles.sns}>
+        <div
+          onClick={() =>
+            openLink("https://www.facebook.com/profile.php?id=100073682116389")
+          }
+        >
+          <Image src="/images/icon-fb.png" width="70" height="70" />
         </div>
-        <div onClick={() => openLink("https://andongjob.kr/")}>
-          주관 ·
-          <div className={styles.localeImage}>
-            <Image src="/images/icon-locale2.png" width="139" height="33" />
-          </div>
+        <div
+          onClick={() => openLink("https://www.instagram.com/beta_festival_/")}
+        >
+          <Image src="/images/icon-insta.png" width="70" height="70" />
+        </div>
+        <div
+          onClick={() =>
+            openLink("https://www.youtube.com/channel/UCX7IRsH1eGOruoGZhY-kebw")
+          }
+        >
+          <Image src="/images/icon-youtube.png" width="70" height="70" />
         </div>
       </div>
       <div className={styles.info}>
         <div>(안동형 일자리 사업단)</div>
         <div>전화번호 : 070-4174-0009 이메일: betafesta@gmail.com</div>
         <div>COPYRIGHT © 2021 안동형 일자리 BETA페스티벌</div>
+      </div>
+    </div>
+    <div>
+      <div className={styles.locale}>
+        <div>
+          <div className={styles.title}>주최</div>
+          <div
+            className={styles.symbol}
+            onClick={() => openLink("https://www.andong.go.kr/main.do")}
+          >
+            <Image src="/images/icon-locale1.png" width="139" height="33" />
+          </div>
+        </div>
+        <div>
+          <div className={styles.title}>주관</div>
+          <div
+            className={styles.symbol}
+            onClick={() => openLink("https://andongjob.kr/")}
+          >
+            <Image src="/images/image-subject.png" width="186" height="33" />
+          </div>
+        </div>
+      </div>
+      <div className={styles.support}>
+        <div className={styles.title}>후원</div>
+        <div className={styles.supportSymbol}>
+          <div className={styles.symbol}>
+            <Image src="/images/image-support1.png" width="165" height="33" />
+          </div>
+          <div className={styles.symbol}>
+            <Image src="/images/image-support2.png" width="159" height="33" />
+          </div>
+          <div className={styles.symbol}>
+            <Image src="/images/image-support3.png" width="138" height="33" />
+          </div>
+        </div>
       </div>
     </div>
   </div>
--- components/pages/ideaton/Section1.tsx
+++ components/pages/ideaton/Section1.tsx
@@ -11,14 +11,18 @@ export const Section1 = () => {
         <div className={styles.content}>
           <div className={styles.title}>『2021 BETA 페스티벌』 아이디어톤</div>
           <div className={styles.info}>
-            <div>일시</div>
+            <div>일 시</div>
             <div>2021. 11. 18. (목) ~ 2021. 11. 19. (금)</div>
-            <div>접수기간</div>
+            <div>접수 기간</div>
             <div>2021. 10. 29. (금) ~ 2021. 11. 12. (금)</div>
+            <div>주최 주관</div>
+            <div>안동시 / 안동대학교</div>
             <div>참가 구분</div>
             <div>예비창업자 / 창업기업(스타트업)</div>
             <div>평가 장소</div>
             <div>안동그랜드호텔(특설무대)</div>
+            <div>총 상 금</div>
+            <div>예비창업자 1천만 원 / 스타트업 5천만 원</div>
           </div>
           <div className={styles.buttons}>
             <div>공고문 다운받기</div>

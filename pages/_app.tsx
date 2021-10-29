afc05f78dbe094a81b532763e0c15d1851fa0c7c
baekjiuk <jiuk205@naver.com>
2021-10-29 10:24:42 +0900

    modify meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,15 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="title" content="Beta Festival" />
+        <meta
+          property="keyword"
+          content="Beta Festival, 베타페스타, 안동 베타페스타, 안동 베타, betafesta, 베타 페스티벌, 베타페스티벌"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.betafesta.kr" />
+        <meta property="og:image" content="/images/logo-color.png" />
+        <meta property="og:description" content="2021 베타페스티벌 - BetaFestival" />
         <title>베타페스티벌 - Beta Festival</title>
       </Head>
       <script src="/smarteditor/js/service/HuskyEZCreator.js" charSet="utf-8"></script>

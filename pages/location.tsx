ff8fa8bd0a045123634f2adae4015d6226b51690
baekjiuk <jiuk205@naver.com>
2021-11-08 19:20:30 +0900

    add bus

--- pages/location.tsx
+++ pages/location.tsx
@@ -23,16 +23,43 @@ const Location = () => {
     <Layout>
       <div className={styles.container}>
         <div className={styles.display}>
-          <div className={styles.title}>LOCATION</div>
-          <div className={styles.sectionTitle}>
-            <div className={styles.sectionTitleIcon} />
-            오시는 길
-            <span className={styles.address}>
-              안동그랜드호텔 | 경상북도 안동시 성곡동 1569 (우편번호 : 36710)
-            </span>
+          <div className={styles.section}>
+            <div className={styles.title}>LOCATION</div>
+            <div className={styles.sectionTitle}>
+              <div className={styles.sectionTitleIcon} />
+              오시는 길
+              <span className={styles.address}>
+                안동그랜드호텔 | 경상북도 안동시 성곡동 1569 (우편번호 : 36710)
+              </span>
+            </div>
+            <div className={styles.location}>
+              <div id="map" className={styles.locationMap} />
+            </div>
           </div>
-          <div className={styles.location}>
-            <div id="map" className={styles.locationMap} />
+          <div className={styles.section}>
+            <div className={styles.sectionTitle}>
+              <div className={styles.sectionTitleIcon} />
+              무료 셔틀버스
+            </div>
+            <div className={styles.shuttle}>
+              <div className={styles.shuttleInfo}>
+                <div className={styles.shuttleTitle}>· 운행기간</div>
+                <div className={styles.shuttleContent}>11월 18일 ~ 19일 (2일간)</div>
+              </div>
+              <div className={styles.shuttleInfo}>
+                <div className={styles.shuttleTitle}>· 운행시간</div>
+                <div className={styles.shuttleContent}>09:00 ~ 19:00</div>
+              </div>
+              <div className={styles.shuttleInfo}>
+                <div className={styles.shuttleTitle}>· 운행대수</div>
+                <div className={styles.shuttleContent}>1시간당 1대 운행</div>
+              </div>
+              <div className={styles.shuttleInfo}>
+                <div className={styles.shuttleTitle}>· 운행구간</div>
+                <div className={styles.shuttleContent}>모디 684 → 용상사거리 → 안동그랜드호텔</div>
+              </div>
+            </div>
+            <div></div>
           </div>
         </div>
       </div>

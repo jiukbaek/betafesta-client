ac406774d995b117bec396e78e1533a4d6ec362d
baekjiuk <jiuk205@naver.com>
2021-11-12 18:12:40 +0900

    modify schedule

--- components/pages/main/Section3.tsx
+++ components/pages/main/Section3.tsx
@@ -3,6 +3,7 @@ import Image from "components/Image";
 import styles from "./Section3.module.scss";
 
 export const Section3 = () => {
+  const schedules = ["11.18.Thu", "11.19.Fri"];
   return (
     <div id="schedule" className={styles.container}>
       <div className={styles.display}>
@@ -10,14 +11,17 @@ export const Section3 = () => {
           <Image src="/images/section3-title.png" width="1068" height="122" alt="title" />
         </div>
         <div className={styles.schedules}>
-          {new Array(2).fill(null).map((_, index) => (
+          {schedules.map((date, index) => (
             <div key={index} className={styles.schedule}>
-              <Image
-                src={`/images/image-schedule${index + 1}.png`}
-                width="1347"
-                height="1551"
-                alt="schedule"
-              />
+              <div className={styles.scheduleDate}>{date}</div>
+              <div>
+                <Image
+                  src={`/images/image-schedule${index + 1}.png`}
+                  width="1347"
+                  height="1551"
+                  alt="schedule"
+                />
+              </div>
             </div>
           ))}
         </div>

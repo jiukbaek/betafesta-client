db3b7febe98fe4828974061b79549e5cb8666f96
baekjiuk <jiuk205@naver.com>
2021-11-13 00:32:03 +0900

    add consult

--- pages/consult.tsx
+++ pages/consult.tsx
@@ -1,10 +1,68 @@
 import Layout from "components/layout/Layout";
-import Ready from "components/Ready";
+import Image from "components/Image";
 
-const Consult = () => (
-  <Layout>
-    <Ready />
-  </Layout>
-);
+import data from "public/consult.json";
+import styles from "./Consult.module.scss";
+import Button from "components/Button";
+import { useEffect, useState } from "react";
+import useMobile from "util/useMobile";
+
+const Consult = () => {
+  const [active, setActive] = useState(null);
+  const isMobile = useMobile();
+  useEffect(() => {}, [isMobile]);
+
+  return (
+    <Layout>
+      <div className={styles.container}>
+        <div className={styles.display}>
+          <div className={styles.title}>START-UP CONSULTING</div>
+          <div className={styles.company}>
+            {data.consults.length <= 0
+              ? Array(3)
+                  .fill(null)
+                  .map((_, index) => (
+                    <div className={styles.item} key={index}>
+                      <div className={styles.itemArea} />
+                      <div className={styles.itemImage}>
+                        <Image
+                          src="/images/logo-color.png"
+                          width="500"
+                          height="500"
+                          objectFit="contain"
+                        />
+                      </div>
+                      <div className={styles.itemEvent} />
+                      <div className={styles.itemEventMobile} />
+                      <div className={styles.itemActive}>
+                        <div className={styles.itemCompany}>준비 중</div>
+                      </div>
+                    </div>
+                  ))
+              : data.consults.map(({ company, logo }) => (
+                  <div className={styles.item} key={company}>
+                    <div className={styles.itemArea} />
+                    <div className={styles.itemImage}>
+                      <Image src={logo} width="500" height="500" />
+                    </div>
+                    <div className={styles.itemEvent} />
+                    <div className={styles.itemEventMobile} />
+                    <div className={styles.itemActive}>
+                      <div className={styles.itemCompany}>{company}</div>
+                      <Button
+                        className={styles.itemButton}
+                        onClick={console.log}
+                      >
+                        예약하기
+                      </Button>
+                    </div>
+                  </div>
+                ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Consult;
